914f43ebf50fb6c119fbbad601ca5eac89cf4193
Ajithkumar T C <support@tothepoint.com.sg>
2024-10-02 22:43:38 +0530

    updated the api

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// to show whoami
 app.get('/api/whoami', function (req, res) {
   res.json({
     "ipaddress": req.ip,
